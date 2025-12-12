5cce19dd61cb1a722c36f460359fe1feaf008498
Brayan Ortega <brayanortega14245@gmail.com>
2025-12-12 13:20:18 -0500

    arreglos preliminales

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,13 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Medical Supplies - Equipos y Productos Médicos')</title>
+    <!-- AdminLTE CSS -->
+    <link rel="stylesheet" href="{{ asset('css/adminlte.min.css') }}">
+    <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <!-- Custom CSS -->
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     @stack('styles')
 </head>
-<body>
-    @yield('content')
+<body class="hold-transition dark-mode sidebar-mini layout-fixed">
+    <div class="wrapper">
+        @yield('content')
+    </div>
 
+    <!-- AdminLTE JS -->
+    <script src="{{ asset('js/adminlte.min.js') }}"></script>
+    <!-- jQuery (requerido por AdminLTE) -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <!-- Bootstrap JS (requerido por AdminLTE) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     @stack('scripts')
 </body>
 </html>
